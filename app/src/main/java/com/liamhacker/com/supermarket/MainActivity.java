0f52c5a504072c6f449dfce6df248a522654d310
lilingyong <18922292506@163.com>
2016-09-16 09:33:36 +0800

    初始化

---
+++ app/src/main/java/com/liamhacker/com/supermarket/MainActivity.java
@@ -0,0 +1,13 @@
+package com.liamhacker.com.supermarket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
